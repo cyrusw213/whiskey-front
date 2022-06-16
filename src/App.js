6c3a2858f3233cb9576c5d4b6546d0e4fc211751
hudnellmarcus <hudnell.marcus@gmail.com>
2022-06-16 13:04:32 -0700

    added dependencies, added header component setup auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { auth } from './services/firebase'
+import {useState, useEffect} from 'react'
+
+import Header from './components/Header';
 
 function App() {
+  // State to control user 
+  const [user, setUser] = useState(null)
+useEffect(() => {
+  auth.onAuthStateChanged(user => (setUser(user)));
+  // observes changes to the users login state
+})
+  
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header user={user} />
     </div>
   );
 }
+ 
 
 export default App;

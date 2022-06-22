7dae2d10b5efe5456a05758c62253b5d2476028d
trevorfaivre <trevorfaivre@hotmail.com>
2022-06-22 13:48:13 -0500

    add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,22 @@ import { Link } from 'react-router-dom';
 
 import { login, logout } from '../services/firebase'
 
+import { FaGlassWhiskey, FaPenNib, FaHandSpock  } from "react-icons/fa";
+
 const Header = (props) => {
     return (
         <nav className="nav">
             <Link to='/'>
-                <div className="home">Home</div>
+                <div className="home"><FaGlassWhiskey /></div>
+                
             </Link>
             <ul>
                 {
                     props.user
                         //   conditional rendering of login/logout based on user 
-                        ? <li onClick={logout}>Logout</li>
+                        ? <li onClick={logout}><FaHandSpock /></li>
                         :
-                        <li onClick={login}>Login</li>
+                        <li onClick={login}><FaPenNib /></li>
                 }
             </ul>
 
